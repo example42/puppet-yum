832345a7a20f75503b4e9750272617705e411316
Alessandro Franceschi <al@lab42.it>
2012-09-02 20:25:53 +0000

    Added repoforge repo

---
+++ manifests/repo/repoforge.pp
@@ -0,0 +1,12 @@
+class yum::repo::repoforge {
+
+    yum::managed_yumrepo { repoforge:
+        descr => 'RepoForge packages',
+        baseurl => 'http://wftp.tu-chemnitz.de/pub/linux/dag/redhat/el$releasever/en/$basearch/dag',
+        enabled => 1,
+        gpgcheck => 1,
+        gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag',
+        priority => 30,
+    }
+
+}
