32dac473aa9966057c67bc8efb5314c95e485ab8
Marcel Beck <marcelbeck@outlook.com>
2013-06-29 19:44:09 +0200

    It is necessary to manually replace $releasever with either "5" (for 5.x) or "6" (for 6.x)
    http://wiki.nginx.org/Install
    
    'Due to differences between how CentOS, RHEL, and Scientific Linux populate the $releasever variable, it is necessary to manually replace $releasever with either "5" (for 5.x) or "6" (for 6.x), depending upon your OS version.'

--- manifests/repo/nginx.pp
+++ manifests/repo/nginx.pp
@@ -3,10 +3,11 @@
 # This class installs the nginx repo
 #
 class yum::repo::nginx {
+  $osver = split($::operatingsystemrelease, '[.]')
 
   yum::managed_yumrepo { 'nginx':
     descr => 'Nginx official release packages',
-    baseurl => 'http://nginx.org/packages/rhel/$releasever/$basearch/',
+    baseurl => 'http://nginx.org/packages/rhel/${osver[0]}/$basearch/',
     enabled => 1,
     gpgcheck => 0,
     priority => 1,
