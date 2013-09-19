80ea6928de1f328de643a73e01a612fbc671a15f
salderma <salderman1@udayton.edu>
2013-09-19 16:43:03 -0400

    fixing doule quotes

--- manifests/repo/vfabric.pp
+++ manifests/repo/vfabric.pp
@@ -6,11 +6,11 @@ class yum::repo::vfabric {
 
   yum::managed_yumrepo { 'vfabric':
     descr          => 'vFabric 5.2 Repo - $basesearch',
-    baseurl        => 'http://repo.vmware.com/pub/rhel${osver[0]}/vfabric/5.2/$basearch',
+    baseurl        => "http://repo.vmware.com/pub/rhel${osver[0]}/vfabric/5.2/$basearch",
     enabled        => 1,
     gpgcheck       => 1,
     failovermethod => 'priority',
-    gpgkey         => 'http://repo.vmware.com/pub/rhel${osver[0]}/vfabric/5.2/RPM-GPG-KEY-VFABRIC-5.2-EL${osver[0]}',
+    gpgkey         => "http://repo.vmware.com/pub/rhel${osver[0]}/vfabric/5.2/RPM-GPG-KEY-VFABRIC-5.2-EL${osver[0]}",
     priority       => 1,
   }
 
