711d8e3df97f623e7cb1e6cf67f0af2b6dce2040
LukeBakerWgt <luke.baker@wegottickets.com>
2014-02-17 15:06:06 +0000

    Changed mirrorlist url for atomic repositories

--- manifests/repo/atomic.pp
+++ manifests/repo/atomic.pp
@@ -5,7 +5,7 @@
 class yum::repo::atomic {
   yum::managed_yumrepo { 'atomic':
     descr          => 'CentOS / Red Hat Enterprise Linux $releasever - atomicrocketturtle.com',
-    mirrorlist     => 'http://www.atomicorp.com/mirrorlist/atomic/centos-$releasever-$basearch',
+    mirrorlist     => 'http://www.atomicorp.com/channels/mirrorlist/atomic/centos-$releasever-$basearch',
     enabled        => 1,
     gpgcheck       => 1,
     gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY.art',
