044b1fea1b349aaf1d476fb87c505dc17575905a
Andreas Pfaffeneder <apfaffeneder@desktop.zuhause-local.de>
2014-06-27 09:25:37 +0200

    Fixed error in pki-file-source for puppet*-repositories

--- manifests/repo/puppetdevel.pp
+++ manifests/repo/puppetdevel.pp
@@ -14,7 +14,7 @@ class yum::repo::puppetdevel (
     gpgcheck       => 1,
     failovermethod => 'priority',
     gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetlabs',
-    gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-PGDG',
+    gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-puppetlabs',
     priority       => 15,
   }
 
