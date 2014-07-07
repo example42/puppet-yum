60956695d6013a6adca30d6d90432091fba1a456
Markus Burger <markus.burger@jumio.com>
2014-07-07 14:17:27 +0200

    enable gpgchecks for cdh5 and fixed the baseurl

--- manifests/repo/cdh5.pp
+++ manifests/repo/cdh5.pp
@@ -1,11 +1,12 @@
 class yum::repo::cdh5 (
-  $baseurl = http://archive.cloudera.com/cdh5/redhat/6/x86_64/cdh/5/
+  $baseurl = 'http://archive.cloudera.com/cdh5/redhat/6/x86_64/cdh/5/'
 ) {
 
   yum::managed_yumrepo { 'cloudera-cdh5':
     descr          => "Cloudera's Distribution for Hadoop, Version 5",
     baseurl        => $baseurl,
     enabled        => 1,
+    gpgcheck       => 1,
     gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-cloudera',
     gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-cloudera',
     priority       => 20,
