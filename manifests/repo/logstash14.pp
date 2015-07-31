e1d34b92c6941945c2f10313ac6d24936d75f6a3
Gil Brechbuhler <Gil.Brechbuhler@epfl.ch>
2015-07-31 11:47:50 +0200

    add repo for logstashforwarder + doc correction in yum::repo::logstash14

--- manifests/repo/logstash14.pp
+++ manifests/repo/logstash14.pp
@@ -1,6 +1,6 @@
 # = Class: yum::repo::logstash14
 #
-# This class installs the logstash13 repo
+# This class installs the logstash14 repo
 #
 class yum::repo::logstash14 (
   $baseurl = 'http://packages.elasticsearch.org/logstash/1.4/centos',
