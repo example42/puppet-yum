59b8d509e6f0796b5cd8af18c91e45f4661f8969
Paweł Olejniczak <pawel.olejniczak@lppsa.com>
2015-09-11 12:20:13 +0200

    fix lint errors in pgdg94, pgdg95

--- manifests/repo/pgdg94.pp
+++ manifests/repo/pgdg94.pp
@@ -5,13 +5,13 @@
 class yum::repo::pgdg94 {
 
   yum::managed_yumrepo { 'pgdg94':
-    descr          => 'PostgreSQL 9.4 $releasever - $basearch',
-    baseurl        => 'http://yum.postgresql.org/9.4/redhat/rhel-$releasever-$basearch',
-    enabled        => 1,
-    gpgcheck       => 1,
-    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG',
-    gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-PGDG',
-    priority       => 20,
+    descr         => 'PostgreSQL 9.4 $releasever - $basearch',
+    baseurl       => 'http://yum.postgresql.org/9.4/redhat/rhel-$releasever-$basearch',
+    enabled       => 1,
+    gpgcheck      => 1,
+    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG',
+    gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-PGDG',
+    priority      => 20,
   }
 
 }
--- manifests/repo/pgdg95.pp
+++ manifests/repo/pgdg95.pp
@@ -5,13 +5,13 @@
 class yum::repo::pgdg95 {
 
   yum::managed_yumrepo { 'pgdg95':
-    descr          => 'PostgreSQL 9.5 $releasever - $basearch',
-    baseurl        => 'http://yum.postgresql.org/9.5/redhat/rhel-$releasever-$basearch',
-    enabled        => 1,
-    gpgcheck       => 1,
-    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG',
-    gpgkey_source  => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-PGDG',
-    priority       => 20,
+    descr         => 'PostgreSQL 9.5 $releasever - $basearch',
+    baseurl       => 'http://yum.postgresql.org/9.5/redhat/rhel-$releasever-$basearch',
+    enabled       => 1,
+    gpgcheck      => 1,
+    gpgkey        => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG',
+    gpgkey_source => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-PGDG',
+    priority      => 20,
   }
 
 }
