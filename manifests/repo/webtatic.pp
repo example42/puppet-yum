9ee82529b4ac8bba35387ca5b70e5511f5b06691
Baptiste Grenier <bgrenier@gnubila.fr>
2015-10-15 16:30:14 +0200

    osver is a string not an integer and it breaks with future parser.

--- manifests/repo/webtatic.pp
+++ manifests/repo/webtatic.pp
@@ -5,19 +5,19 @@
 class yum::repo::webtatic {
   $osver = split($::operatingsystemrelease, '[.]')
   $mirrorlist = $osver[0] ? {
-    5 => 'http://repo.webtatic.com/yum/centos/5/$basearch/mirrorlist',
-    6 => 'http://repo.webtatic.com/yum/el6/$basearch/mirrorlist',
-    7 => 'http://repo.webtatic.com/yum/el7/$basearch/mirrorlist',
+    '5' => 'http://repo.webtatic.com/yum/centos/5/$basearch/mirrorlist',
+    '6' => 'http://repo.webtatic.com/yum/el6/$basearch/mirrorlist',
+    '7' => 'http://repo.webtatic.com/yum/el7/$basearch/mirrorlist',
   }
   $gpgkey = $osver[0] ? {
-    7       => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-webtatic-el7',
+    '7'     => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-webtatic-el7',
     default => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-webtatic-andy',
   }
   $gpgkey_source = $osver[0] ? {
-    7       => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-webtatic-el7',
+    '7'     => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-webtatic-el7',
     default => 'puppet:///modules/yum/rpm-gpg/RPM-GPG-KEY-webtatic-andy',
   }
-    
+
   yum::managed_yumrepo { 'webtatic':
     descr         => 'Webtatic Repository $releasever - $basearch',
     mirrorlist    => $mirrorlist,
