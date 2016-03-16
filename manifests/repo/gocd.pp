0214305cfb8ebe843b33fc6483e735846297604d
Tim Birkett <tim@birkett-bros.com>
2016-03-16 11:52:31 +0000

    Fix GoCD repo name.

--- manifests/repo/gocd.pp
+++ manifests/repo/gocd.pp
@@ -1,10 +1,10 @@
 # = Class: yum::repo::gocd
 #
-# This class installs the GO CD repo installation
+# This class installs the GoCD repo installation
 #
 class yum::repo::gocd {
   yum::managed_yumrepo { 'gocd':
-    descr          => 'CloudFlare Packages',
+    descr          => 'GoCD YUM Repository',
     baseurl        => 'https://download.go.cd',
     enabled        => 1,
     gpgcheck       => 1,
