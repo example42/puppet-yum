9e96c405717ce937cf86228a6b096c3b3cddf347
Ricky Cook <mail@thatpanda.com>
2016-08-04 14:14:23 +1000

    Add docker repo manifest

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -33,6 +33,7 @@ class yum::defaults ( ) inherits yum::params {
   if 'elastix' in $yum::extrarepo { include yum::repo::elastix }
   if 'mysql_community' in $yum::extrarepo { include yum::repo::mysql_community }
   if 'dell_omsa' in $yum::extrarepo { include yum::repo::dell_omsa }
+  if 'docker' in $yum::extrarepo { include yum::repo::docker }
 
   if $yum::bool_defaultrepo {
     case $::operatingsystem {
